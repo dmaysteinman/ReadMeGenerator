c3ad78d98c5e3637670ac03704e417e75a6561ba
Danielle Steinman <daniellesteinman@Danielles-MacBook-Air.local>
2020-11-22 20:30:04 -0500

    actually started

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
+// array of questions for user
+const prompts = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a brief description of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How is your project installed?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors to your project?",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+    }
 ];
 
 // function to write README file
